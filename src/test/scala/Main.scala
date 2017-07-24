d3142800d6376a2e5f4df8575fc82770cae487ad
Gabriel <gabfssilva@gmail.com>
2017-07-24 03:07:23 -0300

    First commit

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -1,7 +1,10 @@
-import Main.Person
-
 import org.validation.scala._
 import org.validation.scala.matchers._
+
+case class Person(name: String,
+                  occupation: String,
+                  age: Int,
+                  favoriteNumbers: List[Int])
 
 object validators {
   implicit val personValidator = Validator { person: Person =>
@@ -9,23 +12,18 @@ object validators {
       "name cannot be null"
     } ~ assure(person.occupation is notBlank) {
       "occupation cannot be null"
-    } ~ assure(1 is higherThan(0)) {
-      "age cannot be null"
+    } ~ assure(person.age is higherThan(0)) {
+      "age cannot be lower than 1"
+    } ~ assure(person.favoriteNumbers contains 4) {
+      "favorite numbers must contain number 4"
     }
   }
 }
 
 object Main extends App {
-  case class Person(name: String, occupation: String, age: Int)
-
-  //needed to method validate find the right validator
   import validators.personValidator
 
-  val result = validate(Person(null, null, 1))
-
-  assure(List(1, 2, 3) contains 2) {
-
-  }
+  val result = validate(Person(null, null, 1, List(3, 2, 1)))
 
   result.foreach { v =>
     println(v.message)
